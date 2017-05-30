c284e17d5668feaaea6f3553c8ba163f39a69847
Sweezey <daniel@lyxell.nu>
2017-05-30 12:43:59 +0200

    elias edit 1

--- includes/header.php
+++ includes/header.php
@@ -14,7 +14,35 @@
                 <p class="sidebar_links">Ansök</p>
             </a>
             <a href="main.php">
-                <p class="sidebar_links">Logga in</p>
+                <?php if($_SESSION['id'] == 0){ ?>
+                <a href="main.php">
+                    <p class="sidebar_links">Logga in</p>
+                </a>
+                <?php } ?>
+
+                <?php if($_SESSION['rank'] > 0){ ?>
+                <a href="main.php">
+                    <p class="sidebar_links">Se ansökningar</p>
+                </a>
+                <?php } ?>
+
+                <?php if($_SESSION['rank'] > 0){ ?>
+                <a href="handleAdmin.php">
+                    <p class="sidebar_links">Hantera admins</p>
+                </a>
+                <?php } ?>
+
+                <?php if($_SESSION['rank'] == 0 && $_SESSION['id'] > 0){ ?>
+                <a href="status.php">
+                    <p class="sidebar_links">Se min ansökan</p>
+                </a>
+                <?php } ?>
+
+                <?php if($_SESSION['id'] > 0){ ?>
+                <a href="./run/logout.php">
+                    <p class="sidebar_links">Logga ut</p>
+                </a>
+                <?php } ?>
             </a>
         </div>
         <div class="sidebar_socialmedia">
@@ -22,43 +50,5 @@
             <a href="www.twitter.se"><img src="./images/twitter.png"></a>
             <a href="www.instagram.com"><img src="./images/instagram.png"></a>
         </div>
-        <a href="index.php">
-            <p class="sidebar_header">Gratis för Alla</p>
-        </a>
-        <a href="index.php">
-            <p class="sidebar_links">Hem</p>
-        </a>
-        <a href="form.php">
-            <p class="sidebar_links">Ansök</p>
-        </a>
 
-        <?php if($_SESSION['id'] == 0){ ?>
-        <a href="main.php">
-            <p class="sidebar_links">Logga in</p>
-        </a>
-        <?php } ?>
-
-        <?php if($_SESSION['rank'] > 0){ ?>
-        <a href="main.php">
-            <p class="sidebar_links">Se ansökningar</p>
-        </a>
-        <?php } ?>
-
-        <?php if($_SESSION['rank'] > 0){ ?>
-        <a href="handleAdmin.php">
-            <p class="sidebar_links">Hantera admins</p>
-        </a>
-        <?php } ?>
-
-        <?php if($_SESSION['rank'] == 0 && $_SESSION['id'] > 0){ ?>
-        <a href="status.php">
-            <p class="sidebar_links">Se min ansökan</p>
-        </a>
-        <?php } ?>
-
-        <?php if($_SESSION['id'] > 0){ ?>
-        <a href="./run/logout.php">
-            <p class="sidebar_links">Logga ut</p>
-        </a>
-        <?php } ?>
     </div>
